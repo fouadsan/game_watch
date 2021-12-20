f552b69ad70106f621a56d91215e6a5256e993c2
fouadsan <F.benayad95@gmail.com>
2021-12-20 20:21:09 +0100

    Worked on navbar

---
+++ game-watch/src/utils/constants.js
@@ -0,0 +1,8 @@
+import { Link } from "./models";
+
+export const LINKS = [
+  new Link(1, "home", "/"),
+  new Link(2, "genres", "/"),
+  new Link(3, "services", "/"),
+  new Link(4, "contact", "/"),
+];
---
+++ game-watch/src/utils/models.js
@@ -0,0 +1,9 @@
+class Link {
+  constructor(id, text, url) {
+    this.id = id;
+    this.text = text;
+    this.url = url;
+  }
+}
+
+export { Link };
